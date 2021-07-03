7ad5ed797d9009ef45a2b48b66314f28fb940c36
jackdelv <jackdelv39@gmail.com>
2021-07-03 10:59:35 -0400

    Added response for valid dates and returns the UTC and Unix codes.

--- server.js
+++ server.js
@@ -10,18 +10,25 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/:date', function(req, res, done) {
+  if (req.params.date){
+    var date = new Date(req.params.date);
+  } else {
+    var date = new Date();
+  }
+  if (date.toString() == 'Invalid Date'){
+    res.json({error: date.toString()});
+  } else {
+    res.json({unix: date.now(), utc: date.UTC(), });
+  }
 });
 
 
